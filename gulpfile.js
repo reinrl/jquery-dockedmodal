d3f04c0c90cf5e63fe99d31a57d7f103770b56d2
Rich Rein <richard.rein@yahoo.com>
2017-05-12 09:56:57 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var changelog = require('gulp-conventional-changelog');
 gulp.task('min', function() {
   gulp.src('jquery.modal.js')
     .pipe(uglify({preserveComments: 'license'}))
-    .pipe(rename('jquery.modal.min.js'))
+    .pipe(rename('jquery.dockedmodal.min.js'))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('css', function() {
   gulp.src('jquery.modal.css')
     .pipe(uglifycss())
-    .pipe(rename('jquery.modal.min.css'))
+    .pipe(rename('jquery.dockedmodal.min.css'))
     .pipe(gulp.dest('.'));
 });
 
